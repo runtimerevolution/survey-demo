e9e6ceab1f4844091186685d8fe90e46b4206204
Ashish Kulkarni <ashish@advarisk.com>
2016-08-03 12:42:57 +0530

    fix spelling mistake

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -26,7 +26,7 @@ class AttemptsController < ApplicationController
     @attempt = @survey.attempts.new(params_whitelist)
     @attempt.participant = current_user
     if @attempt.valid? && @attempt.save
-      correct_options_text = @survey.correct_options.present? ? 'Bellow are the correct answers marked in green' : ''
+      correct_options_text = @survey.correct_options.present? ? 'Below are the correct answers marked in green' : ''
       redirect_to attempt_path(@attempt.id), notice: "Thank you for answering #{@survey.name}! #{correct_options_text}"
     else
       build_flash(@attempt)   
